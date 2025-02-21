49695bf1823aea0c7fa81c5f8c8304a605570d5d
dmssj <letrey19@gmail.com>
2025-02-21 16:02:08 +0700

    main

---
+++ lab_1502.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("номер задачи:");
+        Console.WriteLine("1 - возведение числа в степень");
+        Console.WriteLine("2 - преобразование числа");
+        Console.Write("введите 1 или 2: ");
