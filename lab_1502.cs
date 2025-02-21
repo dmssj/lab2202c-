efce1e9dde6f16ed50d80141bfb6cfb332972d7e
dmssj <letrey19@gmail.com>
2025-02-21 16:05:15 +0700

    1 задача

--- lab_1502.cs
+++ lab_1502.cs
@@ -9,3 +9,32 @@ class Program
         Console.WriteLine("1 - возведение числа в степень");
         Console.WriteLine("2 - преобразование числа");
         Console.Write("введите 1 или 2: ");
+        
+        int selection = int.Parse(Console.ReadLine());
+
+        if (selection == 1)
+        {
+            elevate();
+        }
+        else if (selection == 2)
+        {
+            convert_number();
+        }
+    }
+
+    static void elevate()
+    {
+        Console.Write("число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.Write("степень: ");
+        int degree = int.Parse(Console.ReadLine());
+
+        int result = 1;
+        for (int i = 0; i < degree; i++)
+        {
+            result *= number;
+        }
+
+        Console.WriteLine($"результат: {result}");
+    }
